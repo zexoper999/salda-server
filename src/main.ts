d69dd0f3db0c40b66eaa8995f7480447a1875d37
jake <zexoper@naver.com>
2026-02-25 23:10:23 +0900

    feat. cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(process.env.PORT ?? 3000);
+
+  // CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  await app.listen(8080);
 }
 bootstrap();
