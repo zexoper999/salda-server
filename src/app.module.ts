dab0e31a7920e8bc5810e7591ce59b41b931532c
jake <zexoper@naver.com>
2026-03-05 23:35:49 +0900

    feat: PrismaService, AppModule, main.ts 설정 (cookie-parser, CORS, ConfigModule)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    AuthModule, // .env 파일을 전역에서 사용 가능하게 설정
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
+  exports: [PrismaService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // await app.listen(process.env.PORT ?? 3000);
-
+  app.use(cookieParser());
   // CORS 설정
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
   });
 
-  await app.listen(8080);
+  await app.listen(4000);
 }
 bootstrap();
