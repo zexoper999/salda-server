10646b7b6f2f28699ab746fec5b80ea74fe34e38
jake <zexoper@naver.com>
2026-04-03 00:28:44 +0900

    chore: ValidationPipe 전역 등록

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // await app.listen(process.env.PORT ?? 3000);
   app.use(cookieParser());
-  // CORS 설정
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true,
